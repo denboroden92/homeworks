03bc2a7524c18a7353f12db271189847ec773a7b
denis <denboroden92@gmail.com>
2021-10-11 21:10:11 +0400

    Add Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+package com.company;
+public class Main {
+
+    public static void main(String []args) {
+/*этопервая прога*/
+        System.out.println("Привет!!!");// Вывод сообщения на экран
+    }
+}1
