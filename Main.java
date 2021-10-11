fbc03bd603e1d26e5f89c330a2904a4f0070e5ad
denis <denboroden92@gmail.com>
2021-10-11 23:02:39 +0400

    Added text

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
 /*этопервая прога*/
-        System.out.println("Привет всем!!!");        // Вывод сообщения на экран
+        System.out.println("Привет всем!!! Это тестовая программа");        // Вывод сообщения на экран
     }
 }
